ebcc6c87794c8f0919ca8ebf8ab830dd7dba2451
André Inoue <118934334+andreyyinoue@users.noreply.github.com>
2023-05-07 14:36:18 -0300

    Aula 11 e 12 - Condições

---
+++ exercicios/ex008.js
@@ -0,0 +1,7 @@
+let vel = 75
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if (vel >= 60){
+    console.log(`Você ultrapassou o limite de velocidade permitida. MULTADO!`)
+} else {
+    console.log(`Você está dentro do limite de velocidade permitida. Boa viagem!`)
+}
---
+++ exercicios/ex011.js
@@ -0,0 +1,10 @@
+let idade = 70
+if (idade < 16) {
+    console.log(`Não pode votar`)
+} else {
+    if (idade < 18 || idade > 65) {
+        console.log("Voto opcional")
+    } else {
+        console.log("Voto obrigatório")
+    }
+}
