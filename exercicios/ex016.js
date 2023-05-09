ee56a6942706697bfa9358a50edc6720451fef84
André Inoue <118934334+andreyyinoue@users.noreply.github.com>
2023-05-09 13:35:05 -0300

    Correção do exercício 016

--- exercicios/ex016.js
+++ exercicios/ex016.js
@@ -1,26 +1,31 @@
 
 function count() {
     let i = document.getElementById('txti')
-    let inicio = Number(i.value)
     let f = document.getElementById('txtf')
-    let fim = Number(f.value)
     let p = document.getElementById('txtp')
+    let inicio = Number(i.value)
+    let fim = Number(f.value)
     let passo = Number(p.value)
     let res = document.getElementById('resultado')
     res.innerHTML = ""
-    if (i.value.length == 0 || f.value.lenght == 0 || p.value.length == 0 || passo == 0) {
-        alert('Dados incompletos. Preencha todos os dados e tente novamente.')
-    } else {
-        if (passo > 0){
-            for(let c = inicio; c <= fim; c += passo){
-                res.innerHTML += `${c} >  `
-        }
-        } else {
-            for(c = inicio; c >= fim; c += passo)
+    if (i.value.length == 0 || f.value.length == 0 || p.value.length == 0) {
+        window.alert('Dados incompletos. Preencha os dados abaixo, e tente novamente.')
+    } else if (passo == 0) {
+        alert('Impossível contar. Valor mínimo para o PASSO: 1')
+    } else if (inicio < fim) {
+        for(let c = inicio; c <= fim; c += 1) {
             res.innerHTML += `${c} > `
         }
-            
+    } else if(inicio > fim) {
+        for(let c = inicio; c >= fim; c -= passo) {
+            res.innerHTML += `${c} > `
+        }
     }
-    res.innerHTML += "FIM"
-    }
+    res.innerText += " FIM"
+}
 
+//Exemplo de Emoji no JS
+
+/*
+U+1F92A --> `/u{1F92A}` (Apenas entre crase)
+*/
